1e910782f1ab477aa357a7aa29be9fc6ee6ec7d8
Hrimfaxi <outmatch@gmail.com>
2013-06-15 07:26:52 +0800

    open browser until completely connected, and some refactors

--- obfs2SSH.py
+++ obfs2SSH.py
@@ -11,6 +11,8 @@ class WinProxy():
 
 			if addr:
 				_winreg.SetValueEx(key, "ProxyServer", None, _winreg.REG_SZ, addr)
+	def enableSocks5(self, addr):
+		self.enable("socks=" + addr)
 	def disable(self):
 		with _winreg.OpenKey(_winreg.HKEY_CURRENT_USER, r"Software\Microsoft\Windows\CurrentVersion\Internet Settings", 0, _winreg.KEY_ALL_ACCESS) as key:
 			_winreg.SetValueEx(key, "ProxyEnable", None, _winreg.REG_DWORD, 0)
@@ -20,7 +22,10 @@ class WinProxy():
 			proxyEnabled = _winreg.QueryValueEx(key, "ProxyEnable")[0]
 		return proxyServer, proxyEnabled
 
-if sys.platform == 'win32':
+def isWin32():
+	return True if sys.platform == 'win32' else False
+
+if isWin32():
 	import _winreg
 	g_proxy = WinProxy()
 
@@ -71,11 +76,15 @@ class Configure:
 		except NoOptionError as e:
 			self.keyFilePath = None
 
-		assert(self.useForwardOrSocks.upper() == 'FORWARD' or self.useForwardOrSocks.upper() == 'SOCKS')
-		assert(self.clientType.upper() == 'PLINK' or self.clientType.upper() == 'SSH')
+		if self.useForwardOrSocks.upper() != 'FORWARD' and self.useForwardOrSocks.upper() != 'SOCKS':
+			raise RuntimeError("Invalid mode for useForwardOrSocks: %s" % (self.useForwardOrSocks))
+
+		if self.clientType.upper() != 'PLINK' and self.clientType.upper() != 'SSH':
+			raise RuntimeError("Invalid client type: %s" % (self.clientType))
 
 		if self.useForwardOrSocks.upper() == 'SOCKS':
-			assert(self.socksPort)
+			if not self.socksPort:
+				raise RuntimeError("Invalid sock5 port")
 
 g_conf = None
 g_quitting = False
@@ -90,6 +99,7 @@ def convertAddress(address):
 def getSubprocessKwargs():
 	kwargs = {}
 
+	# take control of stdin if host key auth is disabled
 	if g_conf.disableHostkeyAuth:
 		kwargs['stdin'] = subprocess.PIPE
 	
@@ -110,6 +120,7 @@ def runPlinkOrSSH(cmd):
 	logging.info("Executing: %s", cmdStr)
 	p = subprocess.Popen(cmd, **getSubprocessKwargs())
 
+	# write 'yes' if hostkey auth is disabled
 	if g_conf.disableHostkeyAuth:
 		p.stdin.write("yes\n")
 
@@ -123,30 +134,38 @@ g_obfsproxyProcess = None
 def onRetriesDelay(retcode):
 	if not g_quitting:
 		logging.info("Terminated by error code %d, restarting in %d seconds...", retcode, g_conf.retriesInterval)
-		time.sleep(g_conf.retriesInterval)
+		doSleep()
 
-def execThr(cmd):
+def obfsproxyThread(cmd):
 	global g_obfsproxyProcess
 
 	while not g_quitting:
 		cmdStr = " ".join(cmd).strip()
 		logging.info("Executing: %s", cmdStr)
 		g_obfsproxyProcess = subprocess.Popen(cmd, **getSubprocessKwargs())
-
+		g_obfsproxyProcess.communicate()
 		retcode = g_obfsproxyProcess.wait()
 		onRetriesDelay(retcode)
 
-def runCmdInThread(cmd):
-	t = threading.Thread(target=execThr, args=(cmd,))
+def runObfsproxy(cmd):
+	t = threading.Thread(target=obfsproxyThread, args=(cmd,))
 	t.daemon = True
 	t.start()
 
-def checkReachable(ip, port):
+def checkReachable(ip, port, timeout=5, complex=True):
 	try:
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		s.settimeout(1)
+		s.settimeout(timeout)
 		s.connect((ip, port))
+
+		if complex:
+			s.sendall('hello\n')
+			l = s.recv(16)
+
 		s.close()
+
+		if complex and len(l) == 0:
+			return False
 	except socket.error as e:
 		return False
 
@@ -197,12 +216,101 @@ def onSIGTERM(signum , stack_frame):
 	cleanup()
 	sys.exit(0)
 
+def waitUntilConnectionCompleted(conf):
+	if conf.useForwardOrSocks.upper() == 'FORWARD':
+		tempAddr = getHttpForwardAddress(conf)
+	else:
+		tempAddr = getSocks5Address(conf)
+	
+	#logging.debug("trying %s:%d", tempAddr[0], tempAddr[1])
+	while not checkReachable(tempAddr[0], tempAddr[1], complex=False):
+		doSleep()
+	logging.debug('%s:%d connected', tempAddr[0], tempAddr[1])
+
 def openBrowser():
-	time.sleep(8)
+	waitUntilConnectionCompleted(g_conf)
 	import webbrowser
 	iexplore = os.path.join(os.environ.get("PROGRAMFILES", "C:\\Program Files"), "Internet Explorer\\IEXPLORE.EXE")
 	ie = webbrowser.get(iexplore)
 	ie.open(g_conf.startupPage)
+
+def generatePlinkOrSSHCmd(conf):
+	plinkCmd = [ conf.clientPath ]
+
+	if conf.verbose:
+		plinkCmd += [ '-v' ]
+
+	if conf.extraOpts:
+		plinkCmd += conf.extraOpts.split(' ')
+
+	plinkCmd += [ '-N' ]
+
+	if conf.keyFilePath:
+		plinkCmd += [ '-i', conf.keyFilePath ]
+	
+	if conf.useForwardOrSocks.upper() == 'FORWARD':
+		plinkCmd += [ '-L', conf.httpProxyForwardAddr ]
+	else:
+		plinkCmd += [ '-D', '%d'% (conf.socksPort) ]
+
+	plinkCmd += [ '-P' if conf.clientType.upper() == 'PLINK' \
+				else '-p', '%d' % (conf.SSHPort) ] 
+
+	plinkCmd += [ '%s@%s' % (conf.username, conf.SSHHostName) ]
+
+	return plinkCmd
+
+def generateObfsproxyCmd(conf):
+	obfsproxyCmd = [ conf.obfs2Path, conf.obfsProtocol, '--dest=%s:%d' % (conf.obfs2HostName, conf.obfs2Port) ]
+
+	if conf.sharedSecret:
+		obfsproxyCmd += [ '--shared-secret=%s' % (conf.sharedSecret) ]
+
+	obfsproxyCmd += [ 'client', '%s:%d' % (conf.SSHHostName, conf.SSHPort) ]
+
+	return obfsproxyCmd
+
+def setupLogger(conf):
+	if conf.useDaemon:
+		if conf.logFilename:
+			logging.basicConfig(filename=conf.logFilename, level=logging.DEBUG if conf.verbose else logging.INFO, format='%(levelname)s - %(asctime)s %(message)s', datefmt='[%b %d %H:%M:%S]')
+	else:
+		logging.basicConfig(level=logging.DEBUG if conf.verbose else logging.INFO, format='%(levelname)s - %(asctime)s %(message)s', datefmt='[%b %d %H:%M:%S]')
+
+def doSleep(t=0):
+	time.sleep(t if t else g_conf.retriesInterval)
+
+def resolveHostnameAndPort(obfs2Address, sshAddress):
+	while True:
+		try:
+			obfs2Hostname, obfs2Port = convertAddress(obfs2Address)
+			sshHostname, sshPort = convertAddress(sshAddress)
+			break
+		except socket.gaierror as e:
+			logging.info(e)
+			doSleep()
+
+	return obfs2Hostname, obfs2Port, sshHostname, sshPort
+
+def getHttpForwardAddress(conf):
+	# returns http forward address like ['127.0.0.1', 3128]
+	tempAddr = conf.httpProxyForwardAddr.split(':')
+
+	if len(tempAddr) < 4:
+		tempAddr.insert(0, '127.0.0.1')
+
+	tempAddr = tempAddr[0:2]
+
+	# Fixup address if it's 0.0.0.0
+	if tempAddr[0] == '0.0.0.0':
+		tempAddr[0] = '127.0.0.1'
+
+	tempAddr[1] = int(tempAddr[1])
+
+	return tempAddr
+
+def getSocks5Address(conf):
+	return ['localhost', conf.socksPort]
 
 def main():
 	global g_conf
@@ -211,47 +319,30 @@ def main():
 	configFn = parseArgv()
 	g_conf = Configure(configFn)
 	signal.signal(signal.SIGTERM, onSIGTERM)
-
-	if g_conf.useDaemon:
-		if g_conf.logFilename:
-			logging.basicConfig(filename=g_conf.logFilename, level=logging.DEBUG if g_conf.verbose else logging.INFO, format='%(levelname)s - %(asctime)s %(message)s', datefmt='[%b %d %H:%M:%S]')
-	else:
-		logging.basicConfig(level=logging.DEBUG if g_conf.verbose else logging.INFO, format='%(levelname)s - %(asctime)s %(message)s', datefmt='[%b %d %H:%M:%S]')
-
-	while True:
-		try:
-			g_conf.obfs2HostName, g_conf.obfs2Port = convertAddress(g_conf.obfs2Addr)
-			g_conf.SSHHostName, g_conf.SSHPort = convertAddress(g_conf.SSHAddr)
-			break
-		except socket.gaierror as e:
-			logging.info(e)
-
+	setupLogger(g_conf)
+	g_conf.obfs2HostName, g_conf.obfs2Port, g_conf.SSHHostName, g_conf.SSHPort = resolveHostnameAndPort(g_conf.obfs2Addr, g_conf.SSHAddr)
 	del g_conf.obfs2Addr, g_conf.SSHAddr
-	obfsproxyCmd = [ g_conf.obfs2Path, g_conf.obfsProtocol, '--dest=%s:%d' % (g_conf.obfs2HostName, g_conf.obfs2Port) ]
-
-	if g_conf.sharedSecret:
-		obfsproxyCmd += [ '--shared-secret=%s' % (g_conf.sharedSecret) ]
-
-	obfsproxyCmd += [ 'client', '%s:%d' % (g_conf.SSHHostName, g_conf.SSHPort) ]
+	obfsproxyCmd = generateObfsproxyCmd(g_conf)
 
 	if g_conf.useDaemon:
-		if os.name == 'nt':
-			raise RuntimeError('cannot be daemon in Windows')
+		if isWin32():
+			raise RuntimeError('Cannot run as daemon in Windows')
 		daemonize()
 
 	if g_conf.disableObfs2:
 		g_conf.SSHHostName, g_conf.SSHPort = g_conf.obfs2HostName, g_conf.obfs2Port
 	else:
-		runCmdInThread(obfsproxyCmd)
+		runObfsproxy(obfsproxyCmd)
 
-	if g_conf.win32ProxySetting and sys.platform == 'win32':
+	if g_conf.win32ProxySetting and isWin32():
 		if g_conf.useForwardOrSocks.upper() == 'FORWARD':
-			tempAddr = g_conf.httpProxyForwardAddr.split(':')[0:2]
-
-			if tempAddr[0] == '0.0.0.0':
-				tempAddr[0] = '127.0.0.1'
-
+			tempAddr = getHttpForwardAddress(g_conf)
+			tempAddr[1] = '%d'%(tempAddr[1])
 			g_proxy.enable(':'.join(tempAddr))
+		else:
+			tempAddr = getSocks5Address(g_conf)
+			tempAddr[1] = '%d'%(tempAddr[1])
+			g_proxy.enableSocks5(':'.join(tempAddr))
 
 		t = threading.Thread(target=openBrowser)
 		t.daemon = True
@@ -259,35 +350,12 @@ def main():
 
 	while not g_quitting:
 		if not g_conf.disableObfs2:
-			while not checkReachable(g_conf.SSHHostName, g_conf.SSHPort):
-				time.sleep(0.5)
+			while not checkReachable(g_conf.SSHHostName, g_conf.SSHPort, complex=False):
+				doSleep()
 
 			logging.info("Obfsporxy connection %s:%s connected", g_conf.obfs2HostName, g_conf.obfs2Port)
-			
-		plinkCmd = [ g_conf.clientPath ]
-
-		if g_conf.verbose:
-			plinkCmd += [ '-v' ]
-
-		if g_conf.extraOpts:
-			plinkCmd += g_conf.extraOpts.split(' ')
-
-		plinkCmd += [ '-N' ]
-
-		if g_conf.keyFilePath:
-			plinkCmd += [ '-i', g_conf.keyFilePath ]
-		
-		if g_conf.useForwardOrSocks.upper() == 'FORWARD':
-			plinkCmd += [ '-L', g_conf.httpProxyForwardAddr ]
-		else:
-			plinkCmd += [ '-D', '%d'% (g_conf.socksPort) ]
-
-		plinkCmd += [ '-P' if g_conf.clientType.upper() == 'PLINK' \
-					else '-p', '%d' % (g_conf.SSHPort) ] 
-
-		plinkCmd += [ '%s@%s' % (g_conf.username, g_conf.SSHHostName) ]
-
 		try:
+			plinkCmd = generatePlinkOrSSHCmd(g_conf)
 			retcode = runPlinkOrSSH(plinkCmd)
 			onRetriesDelay(retcode)
 		except KeyboardInterrupt as e:
@@ -303,13 +371,13 @@ def cleanup():
 
 	g_quitting = True
 
-	if g_conf.win32ProxySetting and sys.platform == 'win32':
+	if g_conf.win32ProxySetting and isWin32():
 		g_proxy.disable()
 
 	if g_obfsproxyProcess:
 		logging.info("Cleanup Process %d", g_obfsproxyProcess.pid)
 		g_obfsproxyProcess.terminate()
-		time.sleep(1)
+		doSleep()
 
 		if g_obfsproxyProcess.poll() is None:
 			g_obfsproxyProcess.kill()
